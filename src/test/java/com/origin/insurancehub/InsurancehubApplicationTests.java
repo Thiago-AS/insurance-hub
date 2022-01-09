6996629ae321d17615058b3ec636d4732813d79c
Thiago Araujo da Silva <thiagos.araujo09@gmail.com>
2022-01-09 11:24:22 -0300

    Initial commit

---
+++ src/test/java/com/origin/insurancehub/InsurancehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origin.insurancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
